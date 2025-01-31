9d6a64a61aa30b71c5b73640e03564e92895122d
Abdullahi311 <abdullahimuh29@gmail.com>
2025-01-31 21:12:26 +0100

    test: Add battle contract tests

---
+++ tests/battle_test.ts
@@ -0,0 +1,33 @@
+import {
+  Clarinet,
+  Tx,
+  Chain,
+  Account,
+  types
+} from 'https://deno.land/x/clarinet@v1.0.0/index.ts';
+import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
+
+Clarinet.test({
+  name: "Ensure can battle monster",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    let wallet_1 = accounts.get("wallet_1")!;
+    
+    let block = chain.mineBlock([
+      Tx.contractCall(
+        "character",
+        "create-character", 
+        ["Hero1"],
+        wallet_1.address
+      ),
+      Tx.contractCall(
+        "battle",
+        "battle-monster",
+        [types.uint(1)],
+        wallet_1.address
+      )
+    ]);
+
+    assertEquals(block.receipts.length, 2);
+    assertEquals(block.receipts[1].result.expectOk(), true);
+  },
+});
