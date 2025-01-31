326c04701bfcf0153f35113bbc8c51bb36660aed
Abdullahi311 <abdullahimuh29@gmail.com>
2025-01-31 21:12:16 +0100

    test: Add character contract tests

---
+++ tests/character_test.ts
@@ -0,0 +1,53 @@
+import {
+  Clarinet,
+  Tx,
+  Chain,
+  Account,
+  types
+} from 'https://deno.land/x/clarinet@v1.0.0/index.ts';
+import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
+
+Clarinet.test({
+  name: "Ensure can create character",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    let wallet_1 = accounts.get("wallet_1")!;
+    
+    let block = chain.mineBlock([
+      Tx.contractCall(
+        "character",
+        "create-character",
+        ["Hero1"],
+        wallet_1.address
+      )
+    ]);
+    
+    assertEquals(block.receipts.length, 1);
+    assertEquals(block.height, 2);
+    assertEquals(block.receipts[0].result.expectOk(), true);
+  },
+});
+
+Clarinet.test({
+  name: "Ensure can level up character",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    let wallet_1 = accounts.get("wallet_1")!;
+    
+    let block = chain.mineBlock([
+      Tx.contractCall(
+        "character",
+        "create-character",
+        ["Hero1"],
+        wallet_1.address
+      ),
+      Tx.contractCall(
+        "character", 
+        "level-up",
+        [],
+        wallet_1.address
+      )
+    ]);
+
+    assertEquals(block.receipts.length, 2);
+    assertEquals(block.receipts[1].result.expectOk(), true);
+  },
+});
